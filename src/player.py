f5197f4925c2217a81943329d1470062f52f72c4
bryanbilek <you@example.com>
2020-07-09 14:27:24 -0500

    added inventory, append & remove items to Player

--- src/player.py
+++ src/player.py
@@ -2,10 +2,22 @@
 # currently.
 
 class Player:
-    def __init__(self, name, current_room, items = []):
+    def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-        self.items = items
+        self.items = []
     
     def __str__(self):
         return f'name: {self.name}, current_room: {self.current_room}, items: {self.items}'
+
+    def inventory(self):
+        return f'{self.items}'
+    
+    def append_item(self, item):
+        self.items.append(item)
+        return f'{self.items}'
+    
+    def remove_item(self, item):
+        self.items.remove(item)
+        return f'{self.items}'
+
